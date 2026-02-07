13eb899a2c94b516aec965160f1e8a3da8d604e8
Arthurlt974 <Arthur.974.a@gmail.com>
2026-02-07 08:03:29 +0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 from streamlit_autorefresh import st_autorefresh
 
 # --- CONFIGURATION GLOBALE ---
-st.set_page_config(page_title="Arthur Trading Hub", layout="wide")
+st.set_page_config(page_title="AM-Trading", layout="wide")
 st_autorefresh(interval=15000, key="global_refresh")
 
 # --- FONCTIONS DE MISE EN CACHE (Anti-Blocage & Performance) ---
