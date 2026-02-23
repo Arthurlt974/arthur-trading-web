af7a2740530d704096d9b3be29c3aa4da866d771
Arthurlt974 <Arthur.974.a@gmail.com>
2026-02-23 20:33:11 +0400

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ import json
 from websocket import create_connection
 from firebase_auth import render_auth_page, render_user_sidebar, _save_current_session_config
 
-st.write(st.secrets["FIREBASE_API_KEY"])
-
 # ============================================================
 #  FONCTIONS UTILES GLOBALES
 # ============================================================
